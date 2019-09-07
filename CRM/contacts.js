5a18ba7824d8ee33859248b08eccaaf8956cc00f
sukh <sukhveer.sangha@tatacommunications.com>
2019-09-07 19:18:40 +0100

    span added to textarea

--- CRM/contacts.js
+++ CRM/contacts.js
@@ -3,6 +3,27 @@ function contactsScreen(mainID) {
     var initialized = false;
     return {
          init: function() {
+
+            // jQuery mouse click listener to submit button
+            $(screen).find('form input [type="submit"]').click(
+                function(evt){
+                    evt.preventDefault();
+                    // check if form is valid
+                    if ($(evt.target).parents('form')[0].checkValidity()){
+                        var contact = this.serializeForm();
+                        var html = '<td>' + contact.phoneNumber +'</td>' +
+                        '<td>' + contact.emailAddress +'</td>' +
+                        '<td>' + contact.companyName +'</td>' +
+                        '<td><time datetime="' + contact.lastContacted + '">' + 
+                        contact.lastContacted + '</time'> + 
+                        '<div class="overlay">' + contact.notes + '</div></td></tr>';
+                    $(screen).find('table tbody').append(html);
+                    $(screen).find('form :input[name]').val('')
+	                $(screen).find('#contactDetails').hide();
+                    }
+                }.bind(this)
+            );
+
             if (initialized) {
                 return;
             }
@@ -43,15 +64,32 @@ function contactsScreen(mainID) {
             })
 
             // Hover over notes
-            var timeTags = document.getElementsByTagName('time');
-            for(let i = 0; i < timeTags.length; i++){
-                timeTags[i].addEventListener('mouseenter', function(event){
-                    event.target.nextElementSibling.style.display = 'block';
-                })
-                timeTags[i].addEventListener('mouseleave', function(event){
-                    event.target.nextElementSibling.style.display = 'none';
-                })
-            }
+            $(screen).find('tbody').on("mouseenter mouseleave", "td > time", function(evt) {
+                if (evt.type === "mouseenter") {
+                     $(evt.target).siblings('.overlay').slideDown();
+                } else {
+                        $(evt.target).siblings('.overlay').slideUp()
+                }
+            });
+
+            // text count for text area in notes
+            $(screen).find('textarea').keyup(function(evt) {
+                if ($(evt.target).siblings('.textCount')) {
+                    var characters = $(evt.target).val().length;
+                    $(evt.target).siblings('.textCount').text(characters + ' characters');
+                    
+                }
+            });
+
+            // hover color effect with mouseleave and mouseenter on the table rows
+            $(screen).find('tbody').on("mouseenter mouseleave", "tr", function(evt) {
+                if (evt.type === "mouseenter") {
+                     $(evt.target).closest('tr').css('color', 'white');
+                     $(evt.target).closest('tr').css('background', '#3056A0');
+                } else {
+                        $(evt.target).closest('tr').removeAttr('style');
+                } 
+            });
 
             initialized = true;
         },
