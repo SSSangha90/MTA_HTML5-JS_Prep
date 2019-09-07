e84db1b6a56c1e8d8e2ee7b41cba56f9e9e42477
sukh <sukhveer.sangha@tatacommunications.com>
2019-09-07 14:01:35 +0100

    JS func and HTML structuring

--- CRM/contacts.js
+++ CRM/contacts.js
@@ -1,42 +1,36 @@
-const init = () => {
-    console.log('application started');
-
-    document.getElementById('addContact').addEventListener('click', (event) => {
-        event.preventDefault();
-        var contactBlock = document.getElementById('contactDetails');
-        contactBlock.style.display = 'block';
-    })
-
-    var timeElements = document.getElementsByTagName('time');
-           for (var i = 0; i < timeElements.length; i++) {
-             timeElements[i].addEventListener("mouseenter", function(event) {
-	             event.target.nextElementSibling.style.display = 'block';
-             });
-    	     timeElements[i].addEventListener("mouseleave", function(event) {
-	            event.target.nextElementSibling.style.display = 'none';
-             });
-          }       
+function contactsScreen(mainID) {
+    var screen = mainID;
+    var initialized = false;
+    return {
+         init: function() {
+            if (initialized) {
+                return;
+            }
+            var contactName = document.getElementById('contactName')
+            contactName.oninvalid = function(e) {
+	            e.target.setCustomValidity("");
+	           if (e.target.validity.valid == false) {
+	               if (e.target.value.length == 0) {
+	               	   e.target.setCustomValidity("Contact name is required.");
+		           } else if (e.target.value.length < 5) {
+			           e.target.setCustomValidity("Contact name must be at least 5 characters."); 
+		           }
+	           }
+            };
+ 
+            var email = document.getElementById('emailAddress')
+            email.oninvalid = function(e) {
+	            e.target.setCustomValidity("");
+	            if (e.target.validity.valid == false) {
+		            if (e.target.value.length == 0) {
+			            e.target.setCustomValidity("Email is required.");
+		             } else {
+			              e.target.setCustomValidity("Please enter a valid email address."); 
+		             }
+	            }
+            };
+            initialized = true;
+        }
+    };
     
 }
-
-init()
-
-// 1 - select all with classes of overlay
-$('.overlay');
-
-// 2 - select all input fields with name attribute
-$(':input[name]')
-
-// 3 - find form element with name attr = companyName 
-// and assign to var called companySelector
-var companySelector = 
-$('form[name="companyName"]')
-
-// 4 - invoke the find method on companySelector
-// and find all option elems gt 0
-companySelector.find('option:gt(0)')
-
-// 5. find the odd numbered tr elements in the tbody
-// $('tbody tr: odd')
-
-$(':input[required]').siblings('label').css('color', 'red');
